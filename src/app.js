f956e67017b66e2a66e2cf5e7be9dea138ff0476
Asad Ali <mo.asadali007@gmail.com>
2025-11-08 22:36:08 +0530

    Reviore on github

--- src/app.js
+++ src/app.js
@@ -1,7 +1,14 @@
 import express from 'express';
+import cors from 'cors';
 
 
 const app = express();
+
+app.use(cors({
+  origin: 'https://revoire.netlify.app',
+  methods: ['GET', 'POST'],
+  credentials: true
+}));
 
 
 
